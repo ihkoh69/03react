46a72ffb5d9ad94e51272d027e790a04518138be
ihkoh69 <yoimono@naver.com>
2025-04-08 14:28:35 +0900

    .

--- 06react-todo/src/App연습1.jsx
+++ 06react-todo/src/App연습1.jsx
@@ -2,56 +2,74 @@ import React, { useState } from "react";
 
 function App() {
   const todos = [
-    { id: 1, text: "할일 첫번째", completed: true },
-    { id: 2, text: "할일 두번째", completed: false },
-    { id: 3, text: "할일 세번째", completed: false },
+    { id: 1, text: "할일1", completed: true },
+    { id: 2, text: "할일2", completed: false },
+    { id: 3, text: "할일3", completed: false },
   ];
 
   const [todoList, setTodoList] = useState([...todos]);
+  const [input, setInput] = useState("");
 
   function deleteTodo(id) {
-    // alert("deleteTodo : " + id);
-
     setTodoList(
       todoList.filter((item) => {
-        return item.id !== id;
         // return item.id !== id ? item : null;
+        return item.id !== id;
       })
     );
   }
 
-  function addTodo() {
-    alert("addTodo");
+  function addTask() {
+    if (input.trim() === "") {
+      alert("할일을 입력하세요");
+      return;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text: input,
+      completed: false,
+    };
+
+    setTodoList([newTodo, ...todoList]);
+    setInput("");
   }
 
   function toggleTodo(id) {
-    alert("toggleTodo " + id);
+    setTodoList(
+      todoList.map((item) => {
+        return item.id === id ? { ...item, completed: !item.completed } : item;
+      })
+    );
   }
 
   return (
     <>
-      <div className="container m-5 border rounded p-5">
-        <h1 className="text-center">My TodoList App</h1>
-        <div className="flex justify-between items-center m-2 p-2 gap-2 border">
+      <div className="container w-[500px] m-auto border rounded mt-12 p-5">
+        <h1 className="text-center">My Todo App</h1>
+        <div className="flex justify-between items-center m-3 p-3 gap-3 border">
           <input
             type="text"
-            placeholder="할일을 입력하세요"
-            className="flex-1 border p-2 m-2"
+            placeholder="할일을 입력하세요."
+            className="flex-1 border"
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+            value={input}
           />
           <button
-            className="bg-blue-300 text-white rounded m-2 p-2"
-            onClick={addTodo}
+            className="text-white bg-blue-300 rounded-sm m-2 p-1"
+            onClick={addTask}
           >
             추가
           </button>
         </div>
-        <div className="dLine border-b mt-5 mb-5"></div>
         <div>
-          <ul>
+          <ul className="">
             {todoList.map((item) => {
               return (
                 <>
-                  <li className="flex justify-between items-center m-3 p-3 bg-blue-50">
+                  <li className="flex justify-between border-b bg-blue-50 m-3 p-3 rounded">
                     <span
                       onClick={() => {
                         toggleTodo(item.id);
@@ -70,7 +88,7 @@ function App() {
                         deleteTodo(item.id);
                       }}
                     >
-                      X
+                      x
                     </button>
                   </li>
                 </>
