d1f48bd4db616ee6ed6c2733abc59b28aee22713
Steave Inhun Ko <yoimono@naver.com>
2025-07-02 13:19:49 +0900

    .

---
+++ 13project1/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container m-auto bg-mint-500 py-4 px-4'>test
+    <button className='btn'>click</button>
+      <div className="card-body bg-amber-300 shadow shadow-2xl">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default App
